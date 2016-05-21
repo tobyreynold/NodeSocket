42100e41a2c2d9797a5ba947f0f9971bc4197379
liuye03 <liuye03@meituan.com>
2016-05-21 18:18:01 +0800

    change title

--- ChatRoom/routes/index.js
+++ ChatRoom/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index',{title:'express'});
+  res.render('index',{title:'技术讨论区'});
 });
 
 module.exports = router;
